8810b5d2a0bfdef33e81ac3a25d33d648f9212dc
kakas <shrimptrain@gmail.com>
2021-09-14 13:48:44 +0800

    Login with otp_attempt

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,36 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_for_database_authentication(email: sign_in_params[:email])
+    if user.nil? || !user.valid_password?(sign_in_params[:password])
+      flash[:alert] = I18n.t('devise.failure.invalid', authentication_keys: 'Email')
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      return render :new
+    end
+
+    if user.otp_required_for_login && !user.validate_and_consume_otp!(sign_in_params[:otp_attempt])
+      flash[:alert] = 'Invalid code'
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      return render :new
+    end
+
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, user)
+    yield user if block_given?
+    respond_with user, location: after_sign_in_path_for(user)
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 end
